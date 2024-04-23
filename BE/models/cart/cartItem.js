7501f236ff0b710872a6e92c02716d98b0ef1a1d
aogie <nguyenhoang3519@gmail.com>
2024-04-23 08:29:20 +0700

    bug: cannot add to cart

--- BE/models/cart/cartItem.js
+++ BE/models/cart/cartItem.js
@@ -3,8 +3,8 @@ import { Schema, model} from 'mongoose';
 const CartItemSchema = new Schema({
   quantity: { type: Number, required: true },
   cart: { type: Schema.Types.ObjectId, ref: 'Cart' },
-  book: { type: Schema.Types.ObjectId, ref: 'Book'},
-  orders: [{ type: Schema.Types.ObjectId, ref: 'Order' }]
+  book: { type: Schema.Types.ObjectId, ref: 'Book'}
+
 }, { collection: 'cartitem' });
 
 export default model('CartItem', CartItemSchema);
--- BE/services/order/ClientOrderService.js
+++ BE/services/order/ClientOrderService.js
@@ -1,25 +1,25 @@
 import ClientOrderServiceImp from "../../services/order/ClientOrderServiceImp.js";
-const ClientOrderServiceImp = new ClientOrderServiceImp();
+const clientOrderServiceImp = new ClientOrderServiceImp();
 
 class ClientOrderService {
     async createOrder(userId, cartItem, session){
-        const order = await ClientOrderServiceImp.createOrder(userId, cartItem, session);
+        const order = await clientOrderServiceImp.createOrder(userId, cartItem, session);
         return order;
     }
     async getAllOrders(request, session){
-        const orders = await ClientOrderServiceImp.getAllOrders(request, session);
+        const orders = await clientOrderServiceImp.getAllOrders(request, session);
         return orders;
     }
     async getOrderById(orderId, request, session){
-        const order = await ClientOrderServiceImp.getOrderById(orderId, session);
+        const order = await clientOrderServiceImp.getOrderById(orderId, session);
         return order;
     }
     async getOrderByStatus(status, request, session){
-        const orders = await ClientOrderServiceImp.getOrderByStatus(status, session);
+        const orders = await clientOrderServiceImp.getOrderByStatus(status, session);
         return orders;
     }
     async cancelOrder(orderId, request, session){
-        await ClientOrderServiceImp.cancelOrder(orderId, request, session);
+        await clientOrderServiceImp.cancelOrder(orderId, request, session);
     }
 }
 
