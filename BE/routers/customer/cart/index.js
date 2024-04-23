b6706714281b86ab726f9be06e9891bed7cdc508
aogie <nguyenhoang3519@gmail.com>
2024-04-23 08:05:45 +0700

    merge be_hung_test to master and changed cart.get to cart.post for addToCart function

--- BE/routers/customer/cart/index.js
+++ BE/routers/customer/cart/index.js
@@ -3,7 +3,7 @@ import CartController from "../../../controllers/cartController.js";
 const cart = Router();
 const cartController = new CartController();
 cart.get('/getCart', cartController.getCart.bind(cartController));
-cart.get('/add/:bookId/:quantity', cartController.addToCart.bind(cartController));
+cart.post('/add/:bookId/:quantity', cartController.addToCart.bind(cartController));
 cart.post('/delete/:bookId', cartController.removeFromCart.bind(cartController))
 cart.post('/update/:bookId/:quantity',cartController.updateCart.bind(cartController))
 cart.get('/clearAll', cartController.clearCart.bind(cartController))
