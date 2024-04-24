503739c25fccd83de22e20e8c40fbda169d4b5ac
aogie <nguyenhoang3519@gmail.com>
2024-04-24 08:06:33 +0700

    fixed conflict merging be_tphuc_test in AuthenticationService as interface

--- BE/services/Auth/AuthenticationService.js
+++ BE/services/Auth/AuthenticationService.js
@@ -1,18 +1,7 @@
-import User from '../../models/auth/user.js'
-import Bcrypt from 'bcryptjs'
-import ErrorRepsonse from '../../responses/ErrorResponse.js';
-import jwt from 'jsonwebtoken';
-import UserService from '../User/UserService.js';
-const userService = new UserService();
-import crypto from 'crypto'
-import MailService from './MailService.js';
-import TokenRegistration from '../../models/auth/tokenregistration.js';
-import CartService from "../../services/cart/CartServiceImp.js";
-import Cart from "../../models/cart/cart.js";
-const cartService = new CartService();
-class AuthenticationService{
+import AuthenticationServiceImp from './AuthenticationServiceImp.js';
 
-class AuthenticationService {
+
+class AuthenticationService {git 
   constructor() {
     this.authServiceImp = new AuthenticationServiceImp();
   }
