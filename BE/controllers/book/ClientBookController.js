b1be7c9843fb284d9a844bc86e64993a74c47a61
Phuctt <tthanhphuc753@gmail.com>
2024-04-25 01:19:13 +0700

    fix conflict

--- BE/controllers/book/ClientBookController.js
+++ BE/controllers/book/ClientBookController.js
@@ -73,7 +73,8 @@ class ClientBookController {
         }
       return res.status(res.statusCode).json({
           status: res.statusCode,
-          message: errorMessage
+          message: errorMessage,
+          data: null
       })
     }
   }
